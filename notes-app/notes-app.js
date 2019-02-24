cb1bc86b998e256811c7c88c87a54ec29f495d14
Brian Bycynski <high5bri@hotmail.com>
2019-02-24 04:49:18 -0500

    Still working

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,4 +1,4 @@
-const notes = getSavedNotes();
+let notes = getSavedNotes();
 
 const filters = {
   searchText: ''
@@ -7,13 +7,16 @@ const filters = {
 renderNotes(notes, filters);
 
 document.querySelector('#create-note').addEventListener('click', function(e) {
+  const id = uuidv4();
+
   notes.push({
-    id: uuidv4(),
+    id: id,
     title: '',
     body: ''
   });
   saveNotes(notes);
-  renderNotes(notes, filters);
+  // renderNotes(notes, filters);
+  location.assign(`/edit.html#${id}`);
 });
 
 document.querySelector('#search-text').addEventListener('input', function(e) {
@@ -24,3 +27,37 @@ document.querySelector('#search-text').addEventListener('input', function(e) {
 document.querySelector('#filter-by').addEventListener('change', function(e) {
   console.log(e.target.value);
 });
+
+window.addEventListener('storage', function(e) {
+  if (e.key === 'notes') {
+    notes = JSON.parse(e.newValue);
+    renderNotes(notes, filters);
+  }
+});
+
+const now = new Date();
+const timeStamp = now.getTime();
+// console.log(timeStamp);
+
+const myDate = new Date(timeStamp);
+// console.log(myDate);
+
+// console.log(now.getTime());
+// console.log(`Year: ${now.getFullYear()}`);
+// console.log(`Month: ${now.getMonth()}`);
+// console.log(`Day of the month: ${now.getDate()}`);
+// console.log(`Hour: ${now.getHours()}`);
+// console.log(`Minutes: ${now.getMinutes()}`);
+// console.log(`Seconds: ${now.getSeconds()}`);
+
+const dateOne = new Date(`March 1 2018 12:00:00`);
+const dateTwo = new Date();
+
+const dateOneTimeStamp = dateOne.getTime();
+const dateTwoTimeStamp = dateTwo.getTime();
+
+if (dateOneTimeStamp < dateTwoTimeStamp) {
+  console.log(dateOne.toString());
+} else if (dateTwoTimeStamp < dateOneTimeStamp) {
+  console.log(dateTwo.toString());
+}
--- notes-app/notes-function.js
+++ notes-app/notes-function.js
@@ -1,5 +1,3 @@
-console.log(uuidv4());
-
 // Read existing notes from localStorage
 
 const getSavedNotes = function() {
@@ -17,16 +15,31 @@ const saveNotes = function(notes) {
   localStorage.setItem('notes', JSON.stringify(notes));
 };
 
+// Remove a note from the list
+const removeNote = function(id) {
+  const noteIndex = notes.findIndex(function(note) {
+    return note.id === id;
+  });
+  if (noteIndex > -1) {
+    notes.splice(noteIndex, 1);
+  }
+};
+
 // Generate DOM structure for a note
 const generateNoteDom = function(note) {
   const noteEl = document.createElement('div');
-  const textEl = document.createElement('span');
+  const textEl = document.createElement('a');
 
   const button = document.createElement('button');
 
   // Setup the remove note button
   button.textContent = 'x';
   noteEl.appendChild(button);
+  button.addEventListener('click', function() {
+    removeNote(note.id);
+    saveNotes(notes);
+    renderNotes(notes, filters);
+  });
 
   // Setup the note title text
   if (note.title.length > 0) {
@@ -34,7 +47,7 @@ const generateNoteDom = function(note) {
   } else {
     textEl.textContent = 'Unnamed note';
   }
-
+  textEl.setAttribute('href', `/edit.html#${note.id}`);
   noteEl.appendChild(textEl);
 
   return noteEl;
